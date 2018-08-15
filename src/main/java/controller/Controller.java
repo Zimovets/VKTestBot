f59e437680756a6912255bc6897cd64742c7967c
Zimovets Sergey <zimovets_s@ukr.net>
2018-08-15 14:32:24 +0300

    - change Controller class, add showUserButtonField, add onShowUsers() method - change sample.fxml, add showUserButton

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import service.VKService;
 import view.TableView;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,12 +17,24 @@ public class Controller {
     private Button start;
 
     @FXML
+    private Button showUsersButton;
+
+    @FXML
     void onStart(ActionEvent event) {
         VKService vkService = new VKService();
         try {
             vkService.init();
-            new TableView().init();
+            showUsersButton.disableProperty().set(false);
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void onShowUsers(ActionEvent event) {
+        try {
+            new TableView().init();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
